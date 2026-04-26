320f876d60a5be6b17a0bd5b15e5ad5a408a0692
Lakshan Ekanayaka <lakshanekanayakam20@gmail.com>
2026-04-26 12:01:17 +0530

    update

--- shared/backendUrl.js
+++ shared/backendUrl.js
@@ -1,3 +1,3 @@
-export const BACKEND_ORIGIN = "http://107.22.13.222:5000";
+export const BACKEND_ORIGIN = "http://localhost:5000";
 export const BACKEND_API_URL = `${BACKEND_ORIGIN}/api`;
-export const BACKEND_GRAFANA_URL = "http://107.22.13.222:3000";
+export const BACKEND_GRAFANA_URL = "http://localhost:3000";
